e55f7c3f8820ec68e3eb89329542574145894147
PavloMarchenko <dksab33@gmail.com>
2020-09-23 10:05:23 +0300

    home work 18.09

---
+++ js/main.js
@@ -0,0 +1,72 @@
+// Write a JavaScript program which compute, the average marks of the following students Then, this average is used to determine the corresponding grade.
+
+// let points = [80, 77, 68, 88, 95];
+// let average = 0;
+// let totalPoints = 0;
+
+// for (i = 0; i < points.length; i++) {
+//   totalPoints += points[i];
+// }
+// average = totalPoints / points.length;
+
+//   if (average > 90 && average <= 100) {
+//     alert(`Середній бал ${average}, оцінка A`)
+
+//   } else if (average > 80 && average <= 90) {
+//     alert(`Середній бал ${average}, оцінка B`)
+
+//   } else if (average > 70 && average <= 80) {
+//     alert(`Середній бал ${average}, оцінка C`)
+
+//   } else if (average > 60 && average <= 70) {
+//     alert(`Середній бал ${average}, оцінка D`)
+
+//   } else if (average > 50 && average <= 60) {
+//     alert(`Середній бал ${average} оцінка F`)
+    
+//   } else {'Bи не здали тест'}
+
+
+
+
+
+
+// 2. Write a JavaScript program to sum the multiples of 3 or 5 under 1000.
+// let total = 0;
+
+// for (i = 0; i <= 1000; i++) {
+//   if (i % 3 === 0 || i % 5 === 0) {
+//     total += i;
+//   }
+// }
+// console.log(total);
+
+
+
+
+
+// 3. Write a JavaScript program to construct the following pattern, using a nested for loop.
+
+// * 
+
+// * * 
+
+// * * * 
+
+// * * * * 
+
+// * * * * *
+
+
+
+
+// let symbol = '*', space = ' ';
+
+// for (let i = 0; i < 1; i++) {
+//         symbol += space;
+//     for (let j = 0; j < 5; j++) {
+//         console.log(symbol);
+//         symbol += symbol[i] + space;
+
+//     }
+// }
